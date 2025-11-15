61049edb46af8ace7621265ea573c32612e4c16c
Nurkhuzhayeva Shinaray <241243@astanait.edu.kz>
2025-11-15 23:23:50 +0600

    upd

--- src/main/java/adapter/LocalDateTimeAdapter.java
+++ src/main/java/adapter/LocalDateTimeAdapter.java
@@ -1,4 +1,4 @@
-package utils;
+package adapter;
 
 import com.google.gson.*;
 import java.lang.reflect.Type;
--- src/main/java/decorator/TranslateDecorator.java
+++ src/main/java/decorator/TranslateDecorator.java
@@ -1,6 +1,6 @@
 package decorator;
-
 import Classes.Message;
+import java.util.Scanner;
 
 public class TranslateDecorator extends Decorator{
     public TranslateDecorator(SendingTypes sendingTypes){
@@ -9,7 +9,21 @@ public class TranslateDecorator extends Decorator{
 
     @Override
     public Message createMessage() {
-
+        Message message = super.createMessage();
+        String text = message.getText();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose which language you want translate to: ");
+        System.out.println("1.Kazakh");
+        System.out.println("2.Russian");
+        System.out.println("3.English");
+        String choice = sc.nextLine();
+        String translated = switch (choice){
+            case "1" -> "Message was translated to Kazakh";
+            case "2" -> "Message was translated to Russian";
+            case "3" -> "Message was translated to English";
+            default -> text;
+        };
+        message.setText(translated);
         return message;
     }
 }
